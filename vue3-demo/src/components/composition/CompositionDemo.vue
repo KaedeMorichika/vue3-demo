96e48644731f3014a300aa372dacea9a4aba54a9
kaede_morichika <kaede.morichika@onlineconsultant.jp>
2022-10-24 18:06:19 +0900

    できるだけコンポーネントに関係ないメソッドは外に出す

---
+++ vue3-demo/src/components/composition/CompositionDemo.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+import {ref, reactive} from "vue";
+
+export default {
+    name: "CompositionDemo"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue3-demo/src/components/options/OptionsDemo.vue
+++ vue3-demo/src/components/options/OptionsDemo.vue
@@ -2,49 +2,54 @@
     <div style="margin: 50px">
         <div style="width: 1000px; display: flex; justify-content: space-between; padding: 10px;">
             <div>
-                <span>縦：</span><input v-model="rowNum" :disabled="isGaming">
+                <span>縦：</span><input v-model="setting.rowNum" :disabled="gameStatus.isGaming">
             </div>
             <div>
-                <span>横：</span><input v-model="columnNum" :disabled="isGaming">
+                <span>横：</span><input v-model="setting.columnNum" :disabled="gameStatus.isGaming">
             </div>
             <div>
-                <span>エイムサイズ：</span><input v-model="aimSize" :disabled="isGaming">
+                <span>エイムサイズ：</span><input v-model="setting.aimSize" :disabled="gameStatus.isGaming">
             </div>
             <div>
                 <button @click="gameStart">スタート！</button>
             </div>
         </div>
         <div style="display: flex; flex-direction: row;">
-            <div v-for="i in parseInt(rowNum)" :key="i" style="display: flex; flex-direction: column">
+            <div v-for="i in parseInt(setting.rowNum)" :key="i" style="display: flex; flex-direction: column">
                 <AimTarget
-                    v-for="j in parseInt(columnNum)" :key="j"
+                    v-for="j in parseInt(setting.columnNum)" :key="j"
                     :row-index="i - 1" :column-index="j - 1"
-                    :size="aimSize"
+                    :size="setting.aimSize"
                     :marked="isMarkedTarget(i - 1, j - 1)"
                     @click="onButtonClick(i - 1, j - 1)"
                 ></AimTarget>
             </div>
         </div>
-        <div v-show="!isGaming && score !== 0">
-            スコア：{{score}}
+        <div v-show="!gameStatus.isGaming && gameStatus.score !== 0">
+            スコア：{{gameStatus.score}}
         </div>
     </div>
 </template>
 
 <script>
 import AimTarget from "@/components/options/AimTarget.vue";
+import {getRandomInt} from "@/utils/utils.js";
 
 export default {
     name: "OptionsDemo",
     components: {AimTarget},
     data () {
         return {
-            columnNum: 6,
-            rowNum: 6,
-            aimSize: '100px',
-            markedTarget: null,
-            score: 0,
-            isGaming: false
+            setting: {
+                columnNum: 6,
+                rowNum: 6,
+                aimSize: '100px'
+            },
+            gameStatus: {
+                markedTarget: null,
+                score: 0,
+                isGaming: false
+            }
         }
     },
     methods: {
@@ -53,9 +58,9 @@ export default {
             this.game()
         },
         initGame () {
-            this.score = 0
-            this.isGaming = true
-            this.markedTarget = this.generateMarkedTarget()
+            this.gameStatus.score = 0
+            this.gameStatus.isGaming = true
+            this.gameStatus.markedTarget = this.generateMarkedTarget()
         },
         game () {
             // const gameTime = 60000
@@ -65,31 +70,28 @@ export default {
             }, gameTime)
         },
         endGame () {
-            this.isGaming = false
-            this.markedTarget = null
+            this.gameStatus.isGaming = false
+            this.gameStatus.markedTarget = null
         },
         generateMarkedTarget() {
             return {
-                row: this._getRandomInt(this.columnNum - 1),
-                column: this._getRandomInt(this.rowNum - 1)
+                row: getRandomInt(this.setting.columnNum - 1),
+                column: getRandomInt(this.setting.rowNum - 1)
             }
         },
         onButtonClick (row, column) {
             if (this.isMarkedTarget(row, column)) {
-                this.score++
-                this.markedTarget = this.generateMarkedTarget()
+                this.gameStatus.score++
+                this.gameStatus.markedTarget = this.generateMarkedTarget()
             }
         },
-        _getRandomInt(max) {
-            return Math.floor(Math.random() * max);
-        },
         isMarkedTarget (row, column) {
-            if (this.markedTarget) {
-                return row === this.markedTarget.row && column === this.markedTarget.column
+            if (this.gameStatus.markedTarget) {
+                return row === this.gameStatus.markedTarget.row && column === this.gameStatus.markedTarget.column
             } else {
                 return false
             }
-        },
+        }
     }
 }
 </script>
